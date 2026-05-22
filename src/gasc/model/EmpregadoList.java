3c451749448ebc378aa30eb445cce9b001ea6471
Gustavo <gsilvaif2024@gmail.com>
2026-05-22 00:05:40 -0300

    Implementação dos métodos pesquisar(String), obter(int) e obter(String) da classe EmpregadoList

--- src/gasc/model/EmpregadoList.java
+++ src/gasc/model/EmpregadoList.java
@@ -22,19 +22,32 @@ public class EmpregadoList {
 		return empregadoList.add(empregado);
 	}
 	
-//	// Pesquisa pelo nome do empregado na lista.
-//	public Optional<Empregado> pesquisar(String nome) {
-//		
-//	}
-//	
-//	// Obtém um empregado na posição indicada.
-//	public Optional<Empregado> obter(int posicao) {
-//		
-//	}
-//	
-//	// Obtém a posição do empregado na lista se o seu nome estiver cadastrado.
-//	private OptionalInt obter(String nome) {
-//		
-//	}
+	// Pesquisa pelo nome do empregado na lista.
+	public Optional<Empregado> pesquisar(String nome) {
+		OptionalInt posicaoEmpregadoOpt = obter(nome);
+		
+		if(posicaoEmpregadoOpt.isPresent()) {
+			int posicaoEmpregado = posicaoEmpregadoOpt.getAsInt();
+			Optional<Empregado> empregadoOpt = obter(posicaoEmpregado);
+			
+			if(empregadoOpt.isPresent())
+				return empregadoOpt;
+		}
+		
+		return Optional.empty();
+	}
+	
+	// Obtém um empregado na posição indicada.
+	public Optional<Empregado> obter(int posicao) {
+		return (posicao >= 0 && posicao < numeroEmpregados()) ? Optional.of(empregadoList.get(posicao)) : Optional.empty();
+	}
+	
+	// Obtém a posição do empregado na lista se o seu nome estiver cadastrado.
+	private OptionalInt obter(String nome) {
+		for(Empregado empregado : empregadoList)
+			if(empregado.getNome().compareToIgnoreCase(nome) == 0)
+				return OptionalInt.of(empregadoList.indexOf(empregado));
+		return OptionalInt.empty();
+	}
 	
 }
