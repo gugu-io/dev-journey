25ad6f5e33c543fb399a8d100802fa2b10605def
Gustavo <gsilvaif2024@gmail.com>
2026-05-23 16:25:43 -0300

    Adição da biblioteca de auxilio "InputOutput" de inteface gráfica, e da Interface "Constantes"

---
+++ src/gasc/model/util/Constantes.java
@@ -0,0 +1,9 @@
+package gasc.model.util;
+
+public interface Constantes {
+	final String TITULO_MENU_FOLHA_PAGAMENTO = "Folha de pagamento";
+	final String[] MENU_FOLHA_PAGAMENTO = {"Cadastro", "Consulta", "Alteração", "Relatorio", "Sair"};
+	final String TITULO_CADASTRO_FUNCIONARIO = "Cadastro de funcionários";
+	final String[] MENU_TIPO_EMPREGADO = {"Chefe", "Comissionista", "Horista"};
+	final String TITULO_CONSULTA_EMPREGADO = "Consulta de empregados";
+}
